0cc985e4cd4e5396867efa6611d71df47a0c9a42
oselin <pierfrancesco.oselin@gmail.com>
2022-11-26 20:18:32 +0200

    polar histogram completed

--- src/VFH.py
+++ src/VFH.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 
 def magnitude_matrix(LIDAR_readings, confidence_matrix):
@@ -10,15 +11,14 @@ def magnitude_matrix(LIDAR_readings, confidence_matrix):
     m = np.zeros(shape)
 
     # Determine the a-coefficient according to its definition
-    a = np.amin(LIDAR_readings)
+    a = np.amin(LIDAR_readings)**2
 
     # Calculate the discretization ratio
     discr_ratio = LIDAR_readings[0]/(shape[1]/2)
 
     for i in range(shape[0]):
         for j in range(shape[1]):
-            
-            distance = discr_ratio * np.sqrt( (shape[0] - i)**2 + (shape[1] - j)**2 )
+            distance = discr_ratio * np.sqrt( (shape[0] - i)**2 + (shape[1]/2 - j)**2 )
             m[i,j] = a*confidence_matrix[i,j]**2/distance**2
     return m
 
@@ -32,14 +32,11 @@ def polar_histogram(LIDAR_readings, CARTESIAN_readings, angle_range=180, active_
 
     histogram = np.zeros([int(np.floor(angle_range/active_region))])
     robot_pos = [magnitudes.shape[0],magnitudes.shape[1]/2]
-    #print(robot_pos)
-    idx = 0
+
     for i in range(magnitudes.shape[0]):
         for j in range(magnitudes.shape[1]):
-            angle = np.arctan2((j-robot_pos[1]), i-robot_pos[0]) + np.pi
-            #print(angle)
+            angle = np.arctan2(robot_pos[0]-i, j - robot_pos[1])
             histogram[int(np.floor(angle/active_region))] += magnitudes[i,j]
-            
     return histogram
 
 
--- src/reader.py
+++ src/reader.py
@@ -1,37 +1,36 @@
 #! /usr/bin/env python3
-from re import M
 import rospy
-import math
 import numpy as np
 import matplotlib.pyplot as plt
-from math import cos, sin, radians, pi
-import lidar_to_grid_map as lg
 from numpy import inf
-import os
+import os, sys
 from scipy.ndimage.filters import convolve as conv2
-from sensor_msgs.msg import LaserScan  #import of the message from the package
-import sys
+from sensor_msgs.msg import LaserScan  
+
 sys.path.append(".")
 from src.VFH import polar_histogram
-rospy.init_node('reader') #initialization of the node
 
 
+#initialization of the node
+rospy.init_node('reader') 
+
+
+## Plot Settings
 plt.ion()
 fig, ax = plt.subplots()
-
 plt.axis("equal")
 plt.grid(True, which="minor", color="w", linewidth = .6, alpha = 0.5)
 
+
 def dnorm(x, mu, sd):
     return 1 / (np.sqrt(2 * np.pi) * sd) * np.e ** (-np.power((x - mu) / sd, 2) / 2)
+
 
 def gaussian2(sigma, N=None):
 
     if N is None:
         N = 2*np.maximum(4, np.ceil(6*sigma))+1
-
     k = (N - 1) / 2.
-
     xv, yv = np.meshgrid(np.arange(-k, k+1), np.arange(-k, k+1))
 
     # 2D gaussian filter    
@@ -48,27 +47,25 @@ def gaussian2(sigma, N=None):
 
     return g, gx, gy, gxx, gyy, gxy
 
+
 def gaussian_kernel(size, sigma=1, verbose=False):
- 
     kernel_1D = np.linspace(-(size // 2), size // 2, size)
     for i in range(size):
         kernel_1D[i] = dnorm(kernel_1D[i], 0, sigma)
     kernel_2D = np.outer(kernel_1D.T, kernel_1D.T)
- 
     kernel_2D *= 1.0 / kernel_2D.max()
  
     if verbose:
         plt.imshow(kernel_2D, interpolation='none',cmap='gray')
         plt.title("Image")
         plt.show()
- 
     return kernel_2D
+
 
 def get_front(data):
     data   = np.array(data)
-    angles = np.array(list(range(-90,90))) * np.pi/180
-    dist   = np.concatenate(
-                          (np.flip(data[:90]), np.flip(data[270:])),
+    angles = np.array(list(range(0,180))) * np.pi/180
+    dist   = np.concatenate((data[270:], data[:90]),
                           axis = 0
                           )
     return dist, angles
@@ -77,13 +74,14 @@ def get_front(data):
 def occupancy_map(data, resolution=0.1):
 
     grid = np.zeros([70,70])
+
     X = data[0]
     Y = data[1]
     for i in range(len(X)):
-        indx = int(np.floor(X[i]/resolution)) 
-        indy = int(np.floor(Y[i]/resolution)) + 35 
+        indx = 35 + int(np.floor(X[i]/resolution))
+        indy = 70 - int(np.floor(Y[i]/resolution))
         if indx < grid.shape[0] and indy < grid.shape[1] and indx > 0 and indy > 0: grid[indy,indx] = 1
-    #print("Updated")
+
     return grid
 
 
@@ -91,7 +89,7 @@ def occupancy_map(data, resolution=0.1):
 def plotter(ranges):
     dist, ang = get_front(ranges)
     dist = 4*dist
-    dist[dist == inf] = 0.0
+    dist[dist == inf] = 100
 
     ox = dist*np.cos(ang)
     oy = dist*np.sin(ang)
@@ -103,22 +101,21 @@ def plotter(ranges):
     occmpa_uncert = conv2(occmap,g,mode ="reflect")
 
     myhistgram = polar_histogram(dist, occmpa_uncert)    
+
     os.system("clear")
-    #ax.bar(myhistgram)
-    #plt.bar(myhistgram)
-    #yawreso = math.radians(3.1)  # yaw angle resolution [rad]
-    #pmap, minx, maxx, miny, maxy, xyreso = lg.generate_ray_casting_grid_map(ox, oy, xyreso, False)
     print(myhistgram)
-    #print(pmap)
+    """ax.clear()
+    ax.bar(np.arange(36),myhistgram)
+    ax.set_xlim(0,36)
+    ax.set_ylim(0,100)
+    ax.autoscale(False)"""
+    #plt.clf()
+    plt.bar(np.arange(36),myhistgram)
     #plt.imshow(occmpa_uncert,cmap = "PiYG_r")
-    """xyres = np.array(pmap).shape
-    plt.imshow(pmap, cmap = "PiYG_r")""" 
-    #plt.clim(-0.4, 1.4)
-    """plt.gca().set_xticks(np.arange(-.5, xyres[1], 1), minor = True)
-    plt.gca().set_yticks(np.arange(-.5, xyres[0], 1), minor = True)"""
-    #plt.colorbar()
-    #fig.canvas.draw_idle()
-    #plt.show()"""
+    plt.show()
+    
+    #fig.canvas.flush_events()
+
 
 
 def callback(msg):
@@ -127,9 +124,10 @@ def callback(msg):
 
 
 sub = rospy.Subscriber('/scan' , LaserScan , callback)
-
 rate = rospy.Rate(100)
 
-#plt.show(block=True)
+
+plt.show(block=True)
+
 while not rospy.is_shutdown():
     rate.sleep()
